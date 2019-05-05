50bf414282d9ee1a83a61d9d036c34a3966ff7f5
Nuxii <nuxii09@gmail.com>
2019-05-06 00:58:55 +0100

    Removed exceptions.

--- GMXU/GMBridge.vb
+++ GMXU/GMBridge.vb
@@ -69,20 +69,12 @@
                 Return resourceId
             End Get
             Set(newId As String)
-                If (not IsValid(newId)) Then Throw New InvalidGMResourceIdException("id does not follow specification of " & 
-                                                                                    "00000000-0000-0000-0000-000000000000, " &
-                                                                                    "where 0s are any hexadecimal number.")
+                If (not IsValid(newId)) Then Throw New ArgumentException("id does not follow specification of " & 
+                                                                         "00000000-0000-0000-0000-000000000000, " &
+                                                                         "where 0s are any hexadecimal digit.")
                 resourceId = newId
             End Set
         End Property
-    End Class
-
-    Public Class InvalidGMResourceIdException
-        Inherits Exception
-        '' constructor
-        Public Sub New(message As String)
-            MyBase.New("Malformed resource Id: " & message)
-        End Sub
     End Class
 
 End Module
