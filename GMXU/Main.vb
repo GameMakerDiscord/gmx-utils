356fd3a7eec338c61de96950650ae8a073cd21ef
Nuxii <nuxii09@gmail.com>
2019-05-06 01:57:25 +0100

    Separated classes from modules.

---
+++ GMXU/Main.vb
@@ -0,0 +1,12 @@
+﻿Module Main
+
+    Sub Main()
+        Try
+            Dim args As String() = {"a","test"}'Environment.GetCommandLineArgs()
+            Command.Parse(args,1)
+        Catch e As Exception
+            Console.WriteLine("Error: " & e.Message)
+        End Try
+    End Sub
+
+End Module
