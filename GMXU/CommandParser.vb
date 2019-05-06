d95b17ccc8ff79a97d3e76e5a23fef0278f846c3
Nuxii <nuxii09@gmail.com>
2019-05-06 01:24:14 +0100

    Added command parser.

--- GMXU/CommandParser.vb
+++ GMXU/CommandParser.vb
@@ -9,12 +9,24 @@
             '' Has Nothing
         End Sub
         '' methods
-
+        Public Shared Sub Execute(ByRef c As String())
+            Execute(c, 0)
+        End Sub
+        Public Shared Sub Execute(ByRef c As String(), ByVal i As Integer)
+            If (c.Length <= i) Then Throw New ArgumentException("Params must be non-empty.")
+            Dim name As String = c(i)
+            If (Not commands.ContainsKey(name)) Then Throw New ArgumentException("Command " & name & " does not exist.")
+            Dim cmd As Command = commands(name)
+            Dim paramCount As Integer = c.Length - (i + 1)
+            Dim params(paramCount) As String
+            Array.Copy(c, i + 1, params, 0, paramCount)
+            cmd.Execute(name,params)
+        End Sub
     End Class
 
     Public MustInherit Class Command
         '' methods
-        Public Function GetHelp(Byval verbose As String) As String
+        Public Function GetHelp(ByVal verbose As String) As String
             If (Not verbose) Then Return GetHelp()
             Return _
                 "Description:" & vbCrLf & vbTab & GetHelp() & vbCrLf &
@@ -23,7 +35,7 @@
         '' interface
         Public MustOverride Function GetHelp() As String
         Public MustOverride Function GetSyntax() As String
-        Public MustOverride Function Execute()
+        Public MustOverride Function Execute(ByVal name As String, ByVal params As String())
     End Class
 
 End Module
